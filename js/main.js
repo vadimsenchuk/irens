10df34f209c9df109638a72266f8d4b475ec8862
vadimsenchuk <v.o.senchuk@gmail.com>
2019-10-11 09:28:20 +0300

    deploy

--- js/main.js
+++ js/main.js
@@ -1,49 +1,100 @@
-'use strict'
+'use strict';
+
+const firstScriptTag = document.querySelector('script');
+const $tag = document.createElement('script');
+const $popup = document.createElement('div');
+const $video = document.createElement('div');
+const $closePopup = document.createElement('div');
+let videoId = '';
+
+$popup.className = 'popup';
+$video.className = 'popup__video';
+$video.id = 'player';
+$closePopup.className = 'popup__close';
+$tag.src = 'https://www.youtube.com/iframe_api';
+
+firstScriptTag.parentNode.insertBefore($popup, firstScriptTag);
+$popup.appendChild($video);
+$popup.appendChild($closePopup);
 
 fetch('./store/data.json')
-    .then(res => res.json())
-    .then(data => getTemplate(data));
-
+  .then(res => res.json())
+  .then(data => getTemplate(data));
 
 function getTemplate(arr) {
-    fetch('./template/cards.html')
+  fetch('./template/cards.html')
     .then(res => res.text())
     .then(tpl => {
-        const template = _.template(tpl);
-        const card = arr.reduce((a, b) => a + template(b), '');
-        document.querySelector('.container').innerHTML = card;
-        popup();
+      const template = _.template(tpl);
+      const card = arr.reduce((a, b) => a + template(b), '');
+      document.querySelector('.container').innerHTML = card;
+      popup();
     });
-};
-
-function popup () {
-    const buttons = document.querySelectorAll('.card__link');
-
-    buttons.forEach(button => {
-        button.addEventListener('click', (e) => {
-            const firstScriptTag = document.querySelector('script');
-            const popup = document.createElement('div');
-            const video = document.createElement('div');
-            const closePopup = document.createElement('div');
-            const tag = document.createElement('script');
-
-            popup.classList.add('popup')
-            video.classList.add('popup__video')
-            closePopup.classList.add('popup__close')
-            tag.src = "https://www.youtube.com/iframe_api"
-
-            firstScriptTag.parentNode.insertBefore(popup, firstScriptTag);
-            popup.appendChild(video);
-            video.appendChild(closePopup);
-            deletePopup(popup)
-        })
-    })
 }
 
-function deletePopup(elem) {
-    elem.addEventListener('click', ({target}) => {
-        if (target === elem || target.classList.contains('popup__close')) {
-            elem.remove();
-        }
-    })
+function popup() {
+  const buttons = document.querySelectorAll('.card__link');
+  buttons.forEach(button => {
+    showPopup(button);
+  });
+}
+
+function showPopup(elem) {
+  elem.addEventListener('click', () => {
+    videoId = elem.getAttribute('data-video').split('=')[1];
+    $popup.classList.add('visible');
+    firstScriptTag.parentNode.insertBefore($tag, firstScriptTag);
+    hidePopup($popup);
+    if (document.hasChildNodes($tag)) {
+      onYouTubePlayerAPIReady();
+      console.log(getDuration());
+    }
+  });
+}
+
+function hidePopup(elem) {
+  elem.addEventListener('click', ({ target }) => {
+    if (target === elem || target.classList.contains('popup__close')) {
+      elem.classList.remove('visible');
+      clearVideo();
+    }
+  });
+}
+
+let player;
+function onYouTubePlayerAPIReady() {
+  player = new YT.Player('player', {
+    height: '360',
+    width: '640',
+    videoId,
+    events: {
+      onReady: onPlayerReady,
+      onStateChange: onPlayerStateChange
+    }
+  });
+}
+
+function onPlayerReady(event) {
+  event.target.playVideo();
+}
+
+let done = false;
+function onPlayerStateChange(event) {
+  if (event.data == YT.PlayerState.PLAYING && !done) {
+    setTimeout(stopVideo, 6000);
+    done = true;
+  }
+}
+
+function stopVideo() {
+  player.stopVideo();
+}
+
+function clearVideo() {
+  player.destroy();
+}
+
+function getDuration() {
+  const duration = player.getDuration();
+  return duration;
 }
